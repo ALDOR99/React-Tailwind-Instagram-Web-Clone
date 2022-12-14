e846eaaca75c1e37a76e9c7494cc139dea9da1e2
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2022-12-15 00:52:48 +0300

    .1,0
    :alien:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div>Instagram klonu başlama protokolu</div>;
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <div>
+        <img
+          className="w-[380px] h-[581px]"
+          src="https://static.cdninstagram.com/rsrc.php/v3/y4/r/ItTndlZM2n2.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
