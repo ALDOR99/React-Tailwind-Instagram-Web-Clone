2b26d8ce8762ee2516f797c9d5471b91fbdf2613
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2022-12-15 01:15:14 +0300

    .1,0
    :alien:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 function App() {
   return (
     <div className="h-full w-full flex items-center justify-center">
-      <div>
-        <img
-          className="w-[380px] h-[581px]"
-          src="https://static.cdninstagram.com/rsrc.php/v3/y4/r/ItTndlZM2n2.png"
-          alt=""
-        />
-      </div>
+      <div className="w-[380px] h-[581px] bg-logo-pattern bg-[lenght:468.32px_634.15px] bg-[top_left_-46px]"></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "logo-pattern":
+          "url(https://static.cdninstagram.com/rsrc.php/v3/y4/r/ItTndlZM2n2.png)",
+      },
+    },
   },
   plugins: [],
 };
