5a6a0103eeab8e98767b717166cf6c868ddbc222
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2022-12-15 16:19:44 +0300

    .1,1
    :alien:

--- src/App.js
+++ src/App.js
@@ -13,14 +13,29 @@ function App() {
     let images = ref.current.querySelectorAll("img"),
       total = images.length,
       current = 0;
-    setInterval(() => {
+
+    const imageSlider = () => {
+      if (current > 0) {
+        images[current - 1].classList.add("opacity-0");
+      } else {
+        images[total - 1].classList.add("opacity-0");
+      }
+
       images[current].classList.remove("opacity-0");
+
       if (current === total - 1) {
         current = 0;
       } else {
         current += 1;
       }
-    }, 1000);
+    };
+
+    imageSlider(); // imageSlider fonksiyonunu çalıştır // Run imageSlider function
+
+    let interval = setInterval(imageSlider, 3000); // 3 saniyede bir değişim işlemi // Change operation every 3 seconds
+    return () => {
+      clearInterval(interval); // intervali temizleme // clear interval
+    };
   }, [ref]);
 
   return (
@@ -31,20 +46,20 @@ function App() {
           ref={ref}
         >
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
             src=" https://www.instagram.com/static/images/homepage/screenshots/screenshot1-2x.png/cfd999368de3.png"
           />
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot2-2x.png/80b8aebdea57.png"
           />
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3-2x.png/fe2540684ab2.png"
             alt=""
           />
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4-2x.png/8e9224a71939.png"
             alt=""
           />
