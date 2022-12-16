687145d9773c680b562bdf78234645892c8ce8f5
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2022-12-16 15:53:56 +0300

    .1,2
    :alien:

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 export default function Input({ label, type = "text", ...props }) {
   //-------------------------------------------
+  const inputRef = useRef();
   const [show, setShow] = useState(false);
   const [inputType, setType] = useState(type);
 
@@ -12,20 +13,22 @@ export default function Input({ label, type = "text", ...props }) {
   useEffect(() => {
     if (show) {
       setType("text");
+      inputRef.current.focus();
     } else if (type === "password") {
       setType("password");
+      inputRef.current.focus();
     }
   }, [show]);
 
   return (
-    <label className="block relative">
+    <label className="block relative flex bg-zinc-50 border rounded-sm focus-within::border-gray-400">
       <input
+        ref={inputRef}
         required={true}
         type={inputType}
-        className="bg-zinc-50 border outline-none text-xs rounded-sm w-full h-[38px] focus:border-gray-400 px-2 valid:pt-[10px] peer"
+        className=" outline-none text-xs w-full h-[38px]  px-2 valid:pt-[10px] peer"
         {...props}
       />
-
       <small className="absolute top-1/2 left-[9px] cursor-text pointer-events-none text-xs text-gray-500 transition-all peer-valid:text-[10px] peer-valid:top-2.5 -translate-y-1/2">
         {label}
       </small>
@@ -34,7 +37,7 @@ export default function Input({ label, type = "text", ...props }) {
         <button
           type="button"
           onClick={() => setShow((show) => !show)}
-          className="absolute top-0 right-0 h-full flex items-center text-sm font-semibold pr-2"
+          className="h-full flex items-center text-sm font-semibold pr-2"
         >
           {show ? "Hide" : "Show"}
         </button>
